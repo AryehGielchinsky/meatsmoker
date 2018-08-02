108617a07a3344eac1401f06ea6d2b8b584d74dd
Aryeh Gielchinsky <agielchinsky@gmail.com>
2018-08-02 13:25:51 +0300

    sql updates

---
+++ MySQL/add to smoke_session.sql
@@ -0,0 +1,2 @@
+insert into smoke_session (date_time, meat_type, kilos, notes)
+values (now(), 'pargeyot', 2.9, 'stuff ' );
---
+++ MySQL/create table smoke_session.sql
@@ -0,0 +1,8 @@
+create table smoke_session
+	(id int NOT NULL AUTO_INCREMENT,
+	date_time DATETIME,
+	meat_type VARCHAR(255),
+	kilos FLOAT,
+	notes VARCHAR(255),
+	PRIMARY KEY (id)
+	)
