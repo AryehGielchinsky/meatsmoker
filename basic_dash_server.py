8042f8cb48a82539ef3de6fcefa751447e8ffd41
Aryeh Gielchinsky <agielchinsky@gmail.com>
2020-04-14 17:48:47 +0300

    dash site with refresh buttongit add .git add .!

--- basic_dash_server.py
+++ basic_dash_server.py
@@ -1,4 +1,5 @@
 from time import sleep
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import pymysql.cursors
@@ -10,9 +11,11 @@ from my_functions import get_connection
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 
-def read_data(Smoke_Session_ID, read_type = 'PWM'):
+
+def read_data(Smoke_Session_ID, connection, read_type = 'PWM'):
     try:
         if read_type == 'PWM':
             table_name = 'PWM' 
@@ -27,60 +30,75 @@ def read_data(Smoke_Session_ID, read_type = 'PWM'):
                 """.format(table_name, Smoke_Session_ID)
         cursor.execute(sql)
         result = cursor.fetchall()
+        print(len(result))
         return result
     except Exception as inst:
         print('read_data {}'.format(inst) )
-        
+
+
+
+
 
 
 app = dash.Dash(__name__)
     
+
+
+app.layout = html.Div(
+        [
+                dcc.Graph(id='the_graph')
+                ,html.Button('Click To Refresh', id='my-button', n_clicks=0)
+                ]
+        )
+                        
+@app.callback(
+        Output('the_graph', 'figure')
+        , [Input('my-button', 'n_clicks')]
+        )
+def on_click(n_clicks):
+    connection, login_info = get_connection()
+    Smoke_Session_ID = get_smoke_session(connection)
     
-        
-connection, login_info = get_connection()
-Smoke_Session_ID = get_smoke_session(connection)
-
-df=pd.DataFrame( read_data(Smoke_Session_ID, 'smoker_temps') )
-
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                    dict(
-                            x=df['date_time']
-                            ,y=df['temp0']
-                            ,text='smoker'
-                            ,name='smoker'
-                            )
-                    ,dict(
-                            x=df['date_time']
-                            ,y=df['temp1']
-                            ,text='meat1'
-                            ,name='meat1'
-                            )
-                    ,dict(
-                            x=df['date_time']
-                            ,y=df['temp2']
-                            ,text='meat2'
-                            ,name='meat2'
-                            )
-                    
-                    ]
-            ,'layout': dict(
-                    xaxis={'title': 'time'}
-                    ,yaxis={'title': 'temp'}
-                    #,margin={'l': 40, 'b': 40, 't': 10, 'r': 10}
-                    ,legend={'x': 0, 'y': 1}
-                    ,hovermode='closest'
+    print(datetime.now())
+    df=pd.DataFrame( read_data(Smoke_Session_ID, connection, 'smoker_temps') )
+    print(df.iloc[-1].date_time)
+    data = [
+            dict(
+                    x=df['date_time']
+                    ,y=df['temp0']
+                    ,text='smoker'
+                    ,name='smoker'
                     )
+            ,dict(
+                    x=df['date_time']
+                    ,y=df['temp1']
+                    ,text='meat1'
+                    ,name='meat1'
+                    )
+            ,dict(
+                    x=df['date_time']
+                    ,y=df['temp2']
+                    ,text='meat2'
+                    ,name='meat2'
+                    )
+            ]
+    layout = dict(
+            xaxis={'title': 'time'}
+            ,yaxis={'title': 'temp'}
+            ,legend={'x': 0, 'y': 1}
+            ,hovermode='closest'
+            ) 
+    
+    df = None
+    return {'data':data
+            ,'layout':layout
             }
-        )
-        ]
-        )
-
+   
+    
+    
+                        
+                        
 if __name__ == '__main__':
 #    app.run_server(debug=True)
-    app.run_server( host= '0.0.0.0')
+    app.run_server(host= '0.0.0.0')
 
