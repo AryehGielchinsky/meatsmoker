dff6aea1695171e0cb87d45b52db05dfa84524fd
= <agielchinsky@gmail.com>
2020-10-09 00:11:21 +0300

    remove extra folder

--- MySQL/add_to_smoke_session.sql
+++
@@ -1,2 +0,0 @@
-insert into smoke_session (date_time, meat_type, kilos, notes)
-values (now(), 'brisket', 3.8, 'definitly some first cut, maybe some second cut. I plan to wrap at some point' );
--- MySQL/create_table_smoke_session.sql
+++
@@ -1,8 +0,0 @@
-create table smoke_session
-	(smoke_session_id int NOT NULL AUTO_INCREMENT,
-	date_time DATETIME,
-	meat_type VARCHAR(255),
-	kilos FLOAT,
-	notes VARCHAR(255),
-	PRIMARY KEY (id)
-	)
